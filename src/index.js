89107e6f9b1554709f0959d8061765324f7a56ef
pozil <philippe.ozil@gmail.com>
2022-05-25 18:43:38 +0200

    Fixed Fastlify config for Heroku

--- src/index.js
+++ src/index.js
@@ -190,7 +190,7 @@ app.get('/download', (request, response) => {
 // Start the server
 const start = async () => {
     try {
-        await app.listen(3000);
+        await app.listen(process.env.PORT ? process.env.PORT : 3000, '0.0.0.0');
     } catch (err) {
         app.log.error(err);
         process.exit(1);
